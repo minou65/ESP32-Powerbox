cf392f596e33d91288e337eb870d3ccd649396a4
Andy <azogg65@bluewin.ch>
2025-10-12 17:24:56 +0200

    refactor: update MeterData and InverterStatusData structures for improved data types and add status check methods

--- src/inverterhandling.h
+++ src/inverterhandling.h
@@ -12,33 +12,37 @@
 
 // startet mit Adresse 37100, Laenge 39
 struct MeterData {
+    int startAddress = 37100;
+    int dataLength = 39;
     uint16_t meterStatus;              // 37100, 1 Reg, UINT16, Meter status (0: offline, 1: normal)
-    int32_t gridVoltageA;              // 37101, 2 Reg, INT32, V, Gain 10
-    int32_t gridVoltageB;              // 37103, 2 Reg, INT32, V, Gain 10
-    int32_t gridVoltageC;              // 37105, 2 Reg, INT32, V, Gain 10
-    int32_t gridCurrentA;              // 37107, 2 Reg, INT32, A, Gain 100
-    int32_t gridCurrentB;              // 37109, 2 Reg, INT32, A, Gain 100
-    int32_t gridCurrentC;              // 37111, 2 Reg, INT32, A, Gain 100
-    int32_t activePower;               // 37113, 2 Reg, INT32, W, Gain 1
-    int32_t reactivePower;             // 37115, 2 Reg, INT32, Var, Gain 1
-    int16_t powerFactor;               // 37117, 1 Reg, INT16, NA, Gain 1000
-    int16_t gridFrequency;             // 37118, 1 Reg, INT16, Hz, Gain 100
-    int32_t positiveActiveElectricity; // 37119, 2 Reg, INT32, kWh, Gain 100
-    int32_t reverseActivePower;        // 37121, 2 Reg, INT32, kWh, Gain 100
-    int32_t accumulatedReactivePower;  // 37123, 2 Reg, INT32, kVarh, Gain 100
+    float gridVoltageA;                // 37101, 2 Reg, V
+    float gridVoltageB;                // 37103, 2 Reg, V
+    float gridVoltageC;                // 37105, 2 Reg, V
+    float gridCurrentA;                // 37107, 2 Reg, A
+    float gridCurrentB;                // 37109, 2 Reg, A
+    float gridCurrentC;                // 37111, 2 Reg, A
+    float activePower;                 // 37113, 2 Reg, W
+    float reactivePower;               // 37115, 2 Reg, Var
+    float powerFactor;                 // 37117, 1 Reg, NA
+    float gridFrequency;               // 37118, 1 Reg, Hz
+    float positiveActiveElectricity;   // 37119, 2 Reg, kWh
+    float reverseActivePower;          // 37121, 2 Reg, kWh
+    float accumulatedReactivePower;    // 37123, 2 Reg, kVarh
     uint16_t meterType;                // 37125, 1 Reg, UINT16, Meter type (0: singlephase, 1: threephase)
-    int32_t abLineVoltage;             // 37126, 2 Reg, INT32, V, Gain 10
-    int32_t bcLineVoltage;             // 37128, 2 Reg, INT32, V, Gain 10
-    int32_t caLineVoltage;             // 37130, 2 Reg, INT32, V, Gain 10
-    int32_t aPhaseActivePower;         // 37132, 2 Reg, INT32, W, Gain 1
-    int32_t bPhaseActivePower;         // 37134, 2 Reg, INT32, W, Gain 1
-    int32_t cPhaseActivePower;         // 37136, 2 Reg, INT32, W, Gain 1
+    float abLineVoltage;               // 37126, 2 Reg, V
+    float bcLineVoltage;               // 37128, 2 Reg, V
+    float caLineVoltage;               // 37130, 2 Reg, V
+    float aPhaseActivePower;           // 37132, 2 Reg, W
+    float bPhaseActivePower;           // 37134, 2 Reg, W
+    float cPhaseActivePower;           // 37136, 2 Reg, W
     uint16_t meterModelDetection;      // 37138, 1 Reg, UINT16, Meter model detection result
 };
 extern MeterData meterData;
 
 // startet mit Adresse 32064, Laenge 21
 struct InverterPowerData {
+	int startAddress = 32064;
+	int dataLength = 21;
     float inputPower;           // 32064, 2 Reg, kW
     float gridVoltageAB;        // 32066, 1 Reg, V
     float gridVoltageBC;        // 32067, 1 Reg, V
@@ -57,18 +61,32 @@ struct InverterPowerData {
 extern InverterPowerData inverterPowerData;
 
 struct InverterStatusData {
+	// startet mit Adresse 32000, Laenge 7
+	int startAddress = 32000;
+	int dataLength = 7;
     uint16_t state1;      // 32000, 1 Reg, Bitfield16
     uint16_t state2;      // 32002, 1 Reg, Bitfield16
     uint32_t state3;      // 32003, 2 Reg, Bitfield32
     uint16_t alarm1;      // 32008, 1 Reg, Bitfield16
     uint16_t alarm2;      // 32009, 1 Reg, Bitfield16
     uint16_t alarm3;      // 32010, 1 Reg, Bitfield16
+
+    bool isStandby() const { return state1 & (1 << 0); }
+    bool isGridConnected() const { return state1 & (1 << 1); }
+    bool isGridConnectedNormally() const { return state1 & (1 << 2); }
+    bool isDeratingPowerRationing() const { return state1 & (1 << 3); }
+    bool isDeratingInternal() const { return state1 & (1 << 4); }
+    bool isNormalStop() const { return state1 & (1 << 5); }
+    bool isFaultStop() const { return state1 & (1 << 6); }
+    bool isPowerRationingStop() const { return state1 & (1 << 7); }
+    bool isShutdown() const { return state1 & (1 << 8); }
+    bool isSpotCheck() const { return state1 & (1 << 9); }
 };
 extern InverterStatusData inverterStatusData;
 
 
 void setupInverter();
 void loopInverter();
-void requestInverter(String ip, uint16_t port, uint16_t startaddress, uint16_t number);
+void requestInverter(String ip, uint16_t port, uint16_t startaddress, uint16_t number, uint32_t token = millis());
 
 #endif
