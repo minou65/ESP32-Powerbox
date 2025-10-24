dc3768e39a08e09b1b87e81f4b0bff0aa985b570
Andy <azogg65@bluewin.ch>
2025-10-24 12:13:35 +0200

    refactor: optimize consumer power management and simplify enabling logic

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 502
-#define VERSION_DATE "2025-10-17"
-#define VERSION_TIME "07:30:03"
-#define VERSION "1.3.0.502"
-#define VERSION_STR "1.3.0.502 (2025-10-17 07:30:03)"
+#define VERSION_BUILD 530
+#define VERSION_DATE "2025-10-19"
+#define VERSION_TIME "07:11:39"
+#define VERSION "1.3.0.530"
+#define VERSION_STR "1.3.0.530 (2025-10-19 07:11:39)"
