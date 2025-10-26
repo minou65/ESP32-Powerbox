5d433d1d31d178ca280f911fae2a23b6e53aa86e
Andy <azogg65@bluewin.ch>
2025-10-26 12:23:45 +0100

    refactor: enhance Consumer class with improved status handling and partial load management

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_PATCH 10
+#define VERSION_PATCH 11
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 564
+#define VERSION_BUILD 569
 #define VERSION_DATE "2025-10-26"
-#define VERSION_TIME "07:48:03"
-#define VERSION "1.3.10.564"
-#define VERSION_STR "1.3.10.564 (2025-10-26 07:48:03)"
+#define VERSION_TIME "12:20:02"
+#define VERSION "1.3.11.569"
+#define VERSION_STR "1.3.11.569 (2025-10-26 12:20:02)"
--- src/webhandling.h
+++ src/webhandling.h
@@ -45,44 +45,62 @@ extern IotWebConf iotWebConf;
 extern bool ShouldReboot;
 
 class Consumer {
+protected:
+    bool _Enabled = false;
+    Neotimer _timer;
+    bool _partialLoadAllowed = false;
+    float _partialLoadThreshold = 0.5f;
+    char _NameValue[STRING_LEN] = { 0 };
+    char _PowerValue[NUMBER_LEN] = { 0 };
+    char _DelayValue[NUMBER_LEN] = { 0 };
+    char _TimeValue[STRING_LEN] = { 0 };
+
 public:
-    enum Status {
-        Disabled,
-        Enabled,
-		DelayedOff
-    };
+    enum Status { Disabled, Enabled, DelayedOff };
 
     virtual ~Consumer() {}
-    virtual bool isEnabled() const = 0;
-	virtual bool isActive() const { return true; }
-	virtual bool isParitialLoadAllowed() const { return _partialLoadAllowed; }
-    virtual void setEnabled(bool enabled) {};
-    virtual void setPartialLoadAllowed(bool allowed) { _partialLoadAllowed = allowed; };
-    virtual uint32_t getPower() const = 0;
-    virtual String getName() const = 0;
-    virtual Status getStatus() {
-        if (!isEnabled()) {
-            return Disabled;
-        } else {
-			return Enabled;
+
+    // Status und Aktivierung
+    virtual bool isEnabled() const { return _Enabled; }
+    virtual void setEnabled(bool enabled) {
+        if (enabled == _Enabled) return;
+        _Enabled = enabled;
+        if (_Enabled) _timer.stop();
+        else {
+            _timer.set(getDelay() * 60000);
+            _timer.start();
         }
     }
-	virtual float getPartialLoadThreshold() const { return _partialLoadThreshold; }
-    virtual void process() {}
-    virtual void setup() {};
-    virtual void applyDefaultValue() {};
+    virtual Status getStatus() {
+        if (_Enabled) return Enabled;
+        if (!_timer.done() && _timer.started()) return DelayedOff;
+        return Disabled;
+    }
+    virtual bool isActive() const { return getPower() > 0; }
 
-private:
-	bool _partialLoadAllowed = false; // Default: no partial load
-	float _partialLoadThreshold = 0.5;
+    // Partial Load
+    virtual bool isPartialLoadAllowed() const { return _partialLoadAllowed; }
+    virtual void setPartialLoadAllowed(bool allowed) { _partialLoadAllowed = allowed; }
+    virtual float getPartialLoadThreshold() const { return _partialLoadThreshold; }
+    virtual void setPartialLoadThreshold(float threshold) { _partialLoadThreshold = threshold; }
+
+    // Parameter
+    virtual uint32_t getPower() const { return static_cast<uint32_t>(atoi(_PowerValue)); }
+    virtual String getName() const { return String(_NameValue); }
+    virtual uint32_t getDelay() const { return static_cast<uint32_t>(atoi(_DelayValue)); }
+    virtual String getEnableTime() const { return String(_TimeValue); }
+
+    // Hooks für abgeleitete Klassen
+    virtual void process() {}
+    virtual void begin() {}
+	virtual void end() {}
+    virtual void applyDefaultValue() {}
 };
 
 class Relay : public iotwebconf::ParameterGroup, public Consumer {
 public:
     Relay(const char* id, uint8_t gpio)
         : ParameterGroup(id, "Relay"),
-        _Enabled(false),
-        _timer(60000),
         _prevGroup(nullptr),
         _nextGroup(nullptr) {
         snprintf(_NameId, STRING_LEN, "%s-name", id);
@@ -99,33 +117,12 @@ public:
         addItem(&_GpioParam);
         addItem(&_DelayParam);
         addItem(&_TimeParam);
+
+		_Enabled = false;
+
     }
 
-    bool isEnabled() const override { return _Enabled; }
-    bool isActive() const override { return getPower() > 0; }
-    void setEnabled(bool enabled) override { 
-		if (enabled == _Enabled) return;
-        _Enabled = enabled; 
-        if (_Enabled) {
-			_timer.stop();
-        }
-        else {
-            _timer.set(getDelay() * 60000); // Minuten in Millisekunden
-            _timer.start();
-            Serial.printf("Relay %s disabled, will turn off after %u min\n", getName().c_str(), getDelay());
-        }
-    }
     uint8_t getGPIO() const { return static_cast<uint8_t>(atoi(_GpioValue)); }
-    uint32_t getPower() const override { return static_cast<uint32_t>(atoi(_PowerValue)); }
-    uint32_t getDelay() const { return static_cast<uint32_t>(atoi(_DelayValue)); }
-    String getName() const override { return String(_DesignationValue); }
-    String getEnableTime() const { return String(_TimeValue); }
-
-    Status getStatus() override {
-        if (_Enabled) return Enabled;
-        if (!_timer.done() && _timer.started()) return DelayedOff;
-        return Disabled;
-    }
 
     void setNext(Relay* nextGroup) {
         _nextGroup = nextGroup;
@@ -148,7 +145,6 @@ public:
         if (_Enabled && shouldEnable_) {
             digitalWrite(getGPIO(), HIGH);
             _timer.stop();
-            Serial.printf("Relay %s enabled\n", getName().c_str());
         }
 		else if (!_Enabled) {
             if (_timer.done() && _timer.started()) {
@@ -167,12 +163,16 @@ public:
 
     }
 
-    void setup() override {
+    void begin() override {
         pinMode(getGPIO(), OUTPUT);
         digitalWrite(getGPIO(), LOW);
         setEnabled(false);
         _timer.stop();
     }
+
+    void end() override {
+        digitalWrite(getGPIO(), LOW);
+	}
 
     void applyDefaultValue() override {
         _NameParam.applyDefaultValue();
@@ -183,17 +183,13 @@ public:
     }
 
 private:
-    iotwebconf::TextParameter _NameParam = iotwebconf::TextParameter("Designation", _NameId, _DesignationValue, STRING_LEN, _NameDefault);
+    iotwebconf::TextParameter _NameParam = iotwebconf::TextParameter("Designation", _NameId, _NameValue, STRING_LEN, _NameDefault);
     iotwebconf::NumberParameter _PowerParam = iotwebconf::NumberParameter("Power (W)", _PowerId, _PowerValue, NUMBER_LEN, "0", "0..10000", "min='0' max='10000' step='1'");
     iotwebconf::NumberParameter _GpioParam = iotwebconf::NumberParameter("GPIO", _GpioId, _GpioValue, NUMBER_LEN, _GpioDefault, "0..255", "min='0' max='255' step='1'");
     iotwebconf::NumberParameter _DelayParam = iotwebconf::NumberParameter("Power-off delay (minutes)", _DelayId, _DelayValue, NUMBER_LEN, "1", "1..300", "min='1' max='300' step='1'");
     iotwebconf::TimeParameter _TimeParam = iotwebconf::TimeParameter("Do not enable before ", _TimeId, _TimeValue, STRING_LEN, "00:00");
 
-    char _PowerValue[NUMBER_LEN] = { 0 };
     char _GpioValue[NUMBER_LEN] = { 0 };
-    char _DesignationValue[STRING_LEN];
-    char _DelayValue[NUMBER_LEN] = { 0 };
-    char _TimeValue[STRING_LEN];
 
     char _NameDefault[STRING_LEN] = { 0 };
     char _GpioDefault[STRING_LEN] = { 0 };
@@ -203,18 +199,14 @@ private:
     char _DelayId[STRING_LEN] = { 0 };
     char _TimeId[STRING_LEN] = { 0 };
 
-    bool _Enabled;
     Relay* _prevGroup;
     Relay* _nextGroup;
-    Neotimer _timer;
 };
 
 class Shelly : public iotwebconf::ChainedParameterGroup, public Consumer {
 public:
     Shelly(const char* id)
-        : ChainedParameterGroup(id, "Shelly"),
-        _Enabled(false),
-        _timer(60000) {
+        : ChainedParameterGroup(id, "Shelly") {
         snprintf(_NameId, STRING_LEN, "%s-name", getId());
         snprintf(_UrlOnId, STRING_LEN, "%s-urlon", getId());
         snprintf(_UrlOffId, STRING_LEN, "%s-urloff", getId());
@@ -230,16 +222,13 @@ public:
         addItem(&_PowerParam);
         addItem(&_DelayParam);
         addItem(&_TimeParam);
+
+        _Enabled = false;
     }
 
-    bool isEnabled() const override { return _Enabled; }
     bool isActive() const override {
         return ChainedParameterGroup::isActive() && getPower() > 0;
     }
-    void setEnabled(bool enabled) override { _Enabled = enabled; }
-    uint32_t getPower() const override { return static_cast<uint32_t>(atoi(_PowerValue)); }
-    uint32_t getDelay() const { return static_cast<uint32_t>(atoi(_DelayValue)); }
-	String getName() const { return String(_DesignationValue); }
 
     void process() override {
         if (!isActive()) return;
@@ -256,7 +245,6 @@ public:
         if (_Enabled && shouldEnable_) {
             sendHttpRequest(_UrlOnValue);
             _timer.stop();
-            Serial.printf("Shelly %s enabled, will turn off after %u minutes if power condition met\n", getName().c_str(), getDelay());
         }
         else if (!_Enabled) {
             if (_timer.done() && _timer.started()) {
@@ -267,6 +255,15 @@ public:
         }
     }
 
+    void begin() override {
+        setEnabled(false);
+        _timer.stop();
+	}
+
+    void end() override {
+        sendHttpRequest(_UrlOffValue);
+    }
+
     void applyDefaultValue() override {
         _NameParam.applyDefaultValue();
         _UrlOnParam.applyDefaultValue();
@@ -274,10 +271,10 @@ public:
         _PowerParam.applyDefaultValue();
         _DelayParam.applyDefaultValue();
         _TimeParam.applyDefaultValue();
-	}
+    }
 
 private:
-    iotwebconf::TextParameter _NameParam = iotwebconf::TextParameter("Designation", _NameId, _DesignationValue, STRING_LEN, _NameDefault);
+    iotwebconf::TextParameter _NameParam = iotwebconf::TextParameter("Designation", _NameId, _NameValue, STRING_LEN, _NameDefault);
     iotwebconf::TextParameter _UrlOnParam = iotwebconf::TextParameter("URL on", _UrlOnId, _UrlOnValue, STRING_LEN, "http://");
     iotwebconf::TextParameter _UrlOffParam = iotwebconf::TextParameter("URL off", _UrlOffId, _UrlOffValue, STRING_LEN, "http://");
     iotwebconf::NumberParameter _PowerParam = iotwebconf::NumberParameter("Power (W)", _PowerId, _PowerValue, NUMBER_LEN, "0", "0..10000", "min='0' max='10000' step='1'");
@@ -292,17 +289,10 @@ private:
     char _DelayId[STRING_LEN] = { 0 };
     char _TimeId[STRING_LEN] = { 0 };
 
-    char _PowerValue[NUMBER_LEN] = { 0 };
-    char _DelayValue[NUMBER_LEN] = { 0 };
-    char _DesignationValue[STRING_LEN];
     char _UrlOnValue[STRING_LEN];
     char _UrlOffValue[STRING_LEN];
-    char _TimeValue[STRING_LEN];
-
-    bool _Enabled;
 
     HTTPClient _http;
-    Neotimer _timer;
 
     void sendHttpRequest(const String& url) {
         _http.begin(url);
@@ -313,7 +303,6 @@ private:
         if (httpCode > 0) {
             String payload = _http.getString();
             Serial.printf("Return code is %i\n", httpCode);
-            //Serial.println(payload);
         }
         else {
             Serial.println("Error on HTTP request");
