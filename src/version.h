c0a182feccf2d98f5e9d575d59358ee53ae03fc6
Andy <azogg65@bluewin.ch>
2026-02-19 07:29:41 +0100

    fix: always receive meter data

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 4
-#define VERSION_PATCH 3
+#define VERSION_PATCH 4
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 658
-#define VERSION_DATE "2026-02-09"
-#define VERSION_TIME "13:53:59"
-#define VERSION "1.4.3.658"
-#define VERSION_STR "1.4.3.658 (2026-02-09 13:53:59)"
+#define VERSION_BUILD 663
+#define VERSION_DATE "2026-02-19"
+#define VERSION_TIME "07:09:31"
+#define VERSION "1.4.4.663"
+#define VERSION_STR "1.4.4.663 (2026-02-19 07:09:31)"
